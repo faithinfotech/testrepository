8effb1bb1eef412cae020c65f03bcf16ffcf5e55
KARTHIKA <KARTHIKA@DESKTOP-R90INIG>
2019-09-23 15:14:06 +0530

    initial commit

---
+++ Test2019/src/com/faith/test/Main.java
@@ -0,0 +1,15 @@
+package com.faith.test;
+
+public class Main {
+
+	public static void main(String arg[]){
+		SalesData objSalesData= new SalesData();
+		System.out.println("helloooo...lets start git hub");
+		objSalesData.display();
+	}
+	
+	private static void displayGreetings(){
+		System.out.println("Welcome to sales report app 2019");
+	}
+
+}
---
+++ Test2019/src/com/faith/test/SalesData.java
@@ -0,0 +1,10 @@
+package com.faith.test;
+
+public class SalesData {
+
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("display function is working");
+	}
+
+}
